7a641a46fd1f1a7d4e6d3ae12ba32d0d459d3a18
Cristina Rivera <cristinapruebas14@gmail.com>
2024-01-31 19:01:39 +0100

    Creada migración y modelo de Comentario

---
+++ app/Models/Comentario.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comentario extends Model
+{
+    use HasFactory;
+
+    public function comentable()
+    {
+        return $this->morphTo();
+    }
+
+    public function comentarios()
+    {
+        return $this->morphMany(Comentario::class, 'comentable');
+    }
+
+    public function usuario()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
